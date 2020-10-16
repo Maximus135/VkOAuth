af09b23446b95fb38f0894eb8a5cdc18dbba5fd7
Maxim <maxim.bashilov@yandex.ru>
2020-10-16 14:56:25 +0300

    Updates

---
+++ precache-manifest.539a79186ba0fdbacf3eb2d68ef78717.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f3c85b824ee2f5f53d4e3cf842c5cbe",
+    "url": "/VkOAuth/index.html"
+  },
+  {
+    "revision": "6b19d1f3e95615bdea0e",
+    "url": "/VkOAuth/static/css/main.238c481e.chunk.css"
+  },
+  {
+    "revision": "0533408db25df215c3dc",
+    "url": "/VkOAuth/static/js/2.2bc2fe2c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/VkOAuth/static/js/2.2bc2fe2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b19d1f3e95615bdea0e",
+    "url": "/VkOAuth/static/js/main.8a56a072.chunk.js"
+  },
+  {
+    "revision": "fddb6a6313c3c84d8c33",
+    "url": "/VkOAuth/static/js/runtime-main.c8a10233.js"
+  }
+]);
